390b737034bb477f54e23083c37eda7a348f2317
Luke Harris <6074660+luk707@users.noreply.github.com>
2022-11-27 23:23:53 +0000

    Add forex route and refactor StocksApp composable

--- app/src/main/java/dev/lukeharris/stocks/StocksApp.kt
+++ app/src/main/java/dev/lukeharris/stocks/StocksApp.kt
@@ -1,16 +1,14 @@
 package dev.lukeharris.stocks
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -22,16 +20,29 @@ import androidx.navigation.compose.rememberNavController
 
 sealed class Screen(
     val route: String,
-    val icon: ImageVector,
-    @StringRes val resourceId: Int
+    @DrawableRes val icon: Int,
+    @StringRes val label: Int
 ) {
-    object Stocks : Screen("stocks", Icons.Filled.Home, R.string.stocks)
-    object Settings : Screen("settings", Icons.Filled.Settings, R.string.settings)
+    object Stocks : Screen(
+        "stocks",
+        R.drawable.ic_baseline_show_chart_24,
+        R.string.stocks
+    )
+    object Forex : Screen(
+        "forex",
+        R.drawable.ic_baseline_currency_exchange_24,
+        R.string.forex
+    )
+    object Settings : Screen(
+        "settings",
+        R.drawable.ic_baseline_settings_24,
+        R.string.settings
+    )
 }
 
-val bottomNavigationItems = listOf(
+val bottomNavigationScreens = listOf(
     Screen.Stocks,
-    Screen.Settings
+    Screen.Forex
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,12 +51,40 @@ fun StocksApp() {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     val navController = rememberNavController()
 
+    fun navigateTo(route: String): () -> Unit {
+        return {
+            navController.navigate(route) {
+                // Pop up to the start destination of the graph to
+                // avoid building up a large stack of destinations
+                // on the back stack as users select items
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                // Avoid multiple copies of the same destination when
+                // reselecting the same item
+                launchSingleTop = true
+                // Restore state when reselecting a previously selected item
+                restoreState = true
+            }
+        }
+    }
+
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             LargeTopAppBar(
                 title = { Text("Stocks") },
                 scrollBehavior = scrollBehavior,
+                actions = {
+                    IconButton(onClick = navigateTo(Screen.Settings.route)) {
+                        Icon(
+                            painter = painterResource(id = Screen.Settings.icon),
+                            contentDescription = stringResource(
+                                id = Screen.Settings.label
+                            )
+                        )
+                    }
+                },
             )
         },
         bottomBar = {
@@ -53,35 +92,35 @@ fun StocksApp() {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentDestination = navBackStackEntry?.destination
 
-                bottomNavigationItems.forEach { navigationItem ->
-                    val navigationItemLabel = stringResource(id = navigationItem.resourceId)
+                bottomNavigationScreens.forEach { screen ->
+                    val navigationItemLabel = stringResource(id = screen.label)
 
                     NavigationBarItem(
-                        icon = { Icon(navigationItem.icon, contentDescription = navigationItemLabel) },
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = screen.icon),
+                                contentDescription = navigationItemLabel
+                            )
+                        },
                         label = { Text(navigationItemLabel) },
-                        selected = currentDestination?.hierarchy?.any { it.route == navigationItem.route } == true,
-                        onClick = {
-                            navController.navigate(navigationItem.route) {
-                                // Pop up to the start destination of the graph to
-                                // avoid building up a large stack of destinations
-                                // on the back stack as users select items
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                // Avoid multiple copies of the same destination when
-                                // reselecting the same item
-                                launchSingleTop = true
-                                // Restore state when reselecting a previously selected item
-                                restoreState = true
-                            }
-                        }
+                        selected = currentDestination
+                            ?.hierarchy
+                            ?.any {
+                                it.route == screen.route
+                            } == true,
+                        onClick = navigateTo(screen.route)
                     )
                 }
             }
         },
         content = { innerPadding ->
-            NavHost(navController, startDestination = Screen.Stocks.route, Modifier.padding(innerPadding)) {
+            NavHost(
+                navController,
+                startDestination = Screen.Stocks.route,
+                Modifier.padding(innerPadding)
+            ) {
                 composable(Screen.Stocks.route) { Text("Stocks") }
+                composable(Screen.Forex.route) { Text("Forex") }
                 composable(Screen.Settings.route) { Text("Settings") }
             }
         }
