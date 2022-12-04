ba6292185f58fbcf3e7169ad9ea1f338109b225b
Luke Harris <6074660+luk707@users.noreply.github.com>
2022-12-04 22:25:24 +0000

    Add interactivity to candlestick chart composable

--- app/src/main/java/dev/lukeharris/stocks/CandlestickChart.kt
+++ app/src/main/java/dev/lukeharris/stocks/CandlestickChart.kt
@@ -1,19 +1,37 @@
 package dev.lukeharris.stocks
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.*
+import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import dev.lukeharris.stocks.ui.theme.Cousine
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import java.lang.Double.max
 import java.lang.Double.min
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 data class Candle(
     val c: Double,
@@ -109,6 +127,114 @@ val CANDLESTICK_TEST_DATA = listOf(
     Candle(c = 151.15, h = 151.21, l = 151.11, n = 507, o = 151.17, t = 1669248000000, v = 37437, vw = 151.1548)
 )
 
+suspend fun PointerInputScope.detectTransformGestures(
+    panZoomLock: Boolean = false,
+    consume: Boolean = true,
+    onGestureStart: (PointerInputChange) -> Unit = {},
+    onGesture: (
+        centroid: Offset,
+        pan: Offset,
+        zoom: Float,
+        rotation: Float,
+        mainPointer: PointerInputChange,
+        changes: List<PointerInputChange>
+    ) -> Unit,
+    onGestureEnd: (PointerInputChange) -> Unit = {}
+) {
+    forEachGesture {
+        awaitPointerEventScope {
+            var rotation = 0f
+            var zoom = 1f
+            var pan = Offset.Zero
+            var pastTouchSlop = false
+            val touchSlop = viewConfiguration.touchSlop
+            var lockedToPanZoom = false
+
+
+            // Wait for at least one pointer to press down, and set first contact position
+            val down: PointerInputChange = awaitFirstDown(requireUnconsumed = false)
+            onGestureStart(down)
+
+            var pointer = down
+            // Main pointer is the one that is down initially
+            var pointerId = down.id
+
+            do {
+                val event = awaitPointerEvent()
+
+                // If any position change is consumed from another PointerInputChange
+                val canceled =
+                    event.changes.any { it.isConsumed }
+
+                if (!canceled) {
+
+                    // Get pointer that is down, if first pointer is up
+                    // get another and use it if other pointers are also down
+                    // event.changes.first() doesn't return same order
+                    val pointerInputChange =
+                        event.changes.firstOrNull { it.id == pointerId }
+                            ?: event.changes.first()
+
+                    // Next time will check same pointer with this id
+                    pointerId = pointerInputChange.id
+                    pointer = pointerInputChange
+
+                    val zoomChange = event.calculateZoom()
+                    val rotationChange = event.calculateRotation()
+                    val panChange = event.calculatePan()
+
+                    if (!pastTouchSlop) {
+                        zoom *= zoomChange
+                        rotation += rotationChange
+                        pan += panChange
+
+                        val centroidSize = event.calculateCentroidSize(useCurrent = false)
+                        val zoomMotion = abs(1 - zoom) * centroidSize
+                        val rotationMotion =
+                            abs(rotation * PI.toFloat() * centroidSize / 180f)
+                        val panMotion = pan.getDistance()
+
+                        if (zoomMotion > touchSlop ||
+                            rotationMotion > touchSlop ||
+                            panMotion > touchSlop
+                        ) {
+                            pastTouchSlop = true
+                            lockedToPanZoom = panZoomLock && rotationMotion < touchSlop
+                        }
+                    }
+
+                    if (pastTouchSlop) {
+                        val centroid = event.calculateCentroid(useCurrent = false)
+                        val effectiveRotation = if (lockedToPanZoom) 0f else rotationChange
+                        if (effectiveRotation != 0f ||
+                            zoomChange != 1f ||
+                            panChange != Offset.Zero
+                        ) {
+                            onGesture(
+                                centroid,
+                                panChange,
+                                zoomChange,
+                                effectiveRotation,
+                                pointer,
+                                event.changes
+                            )
+                        }
+
+                        if (consume) {
+                            event.changes.forEach {
+                                if (it.positionChanged()) {
+                                    it.consume()
+                                }
+                            }
+                        }
+                    }
+                }
+            } while (!canceled && event.changes.any { it.pressed })
+            onGestureEnd(pointer)
+        }
+    }
+}
+
 @Preview
 @Composable
 fun CandlestickChart(
@@ -118,36 +244,108 @@ fun CandlestickChart(
     bodyColorPriceDecrease: Color = Color.Red,
     wickColor: Color = Color.Black
 ) {
+    var scale by remember { mutableStateOf(1f) }
+    val offset = remember { Animatable(0f) }
+
+
     Spacer(
         modifier = modifier
+            .pointerInput(Unit) {
+                val decay = splineBasedDecay<Float>(this)
+                val velocityTracker = VelocityTracker()
+
+                coroutineScope {
+                    while (true) {
+                        detectTransformGestures(
+                            onGestureStart = {
+                                velocityTracker.resetTracking()
+                            },
+                            onGesture = { centroid, pan, zoom, _, change, _ ->
+                                val oldScale = scale
+                                var newScale = scale * zoom
+
+                                if (newScale < 1f) {
+                                    newScale = 1f
+                                }
+
+                                var newPosition = (offset.value + centroid.x / oldScale) -
+                                        (centroid.x / newScale + pan.x / oldScale)
+
+                                val lowerBound = 0f
+                                val upperBound = size.width.toFloat() - (size.width.toFloat() / newScale)
+
+                                offset.updateBounds(lowerBound, upperBound)
+
+                                launch {
+                                    offset.snapTo(newPosition)
+                                }
+
+                                scale = newScale
+
+                                velocityTracker.addPosition(
+                                    change.uptimeMillis,
+                                    Offset(x = offset.value, y = 0f)
+                                )
+                            },
+                            onGestureEnd = {
+                                val velocity = velocityTracker.calculateVelocity().x
+                                launch {
+                                    offset.animateDecay(velocity, decay)
+                                }
+                            }
+                        )
+                    }
+                }
+            }
             .drawWithCache {
                 val greenBrush = Brush.verticalGradient(listOf(bodyColorPriceIncrease, bodyColorPriceIncrease))
                 val redBrush = Brush.verticalGradient(listOf(bodyColorPriceDecrease, bodyColorPriceDecrease))
                 val blackBrush = Brush.verticalGradient(listOf(wickColor, wickColor))
 
-                val max = data.map { max(max(it.h, it.o), it.c) }.reduce { acc: Double, p: Double -> max(acc, p) }
-                val min = data.map { min(min(it.l, it.o), it.c) }.reduce { acc: Double, p: Double -> min(acc, p) }
-
-                val range = max - min
+                val candleWidth = this.size.width / data.size * scale
 
                 onDrawBehind {
+                    // TODO: Only calculate bounds from visible candles.
+                    val max = data
+//                        .filterIndexed { index, _ -> index * candleWidth >= offset.value && (index + 1) * candleWidth <= offset.value + size.width / scale }
+                        .map { max(max(it.h, it.o), it.c) }
+                        .reduce { acc: Double, p: Double -> max(acc, p) }
+                    val min = data
+//                        .filterIndexed { index, _ -> index * candleWidth >= offset.value && (index + 1) * candleWidth <= offset.value + size.width / scale }
+                        .map { min(min(it.l, it.o), it.c) }
+                        .reduce { acc: Double, p: Double -> min(acc, p) }
+
+
                     fun candleY(a: Double): Float {
                         return remap(min, max, size.height.toDouble(), 0.0, a).toFloat()
                     }
-                    val candleWidth = this.size.width / data.size
+
+                    // TODO: Only render candles in view
                     data.forEachIndexed { index, candle ->
                         drawLine(
                             blackBrush,
-                            start = Offset(x = (index + 0.5f) * candleWidth, y = candleY(candle.h)),
-                            end = Offset(x = (index + 0.5f) * candleWidth, y = candleY(candle.l)),
-                            cap = StrokeCap.Round,
+                            start = Offset(
+                                x = ((index + 0.5f) * candleWidth) - (offset.value * scale),
+                                y = candleY(candle.h)
+                            ),
+                            end = Offset(
+                                x = ((index + 0.5f) * candleWidth) - (offset.value * scale),
+                                y = candleY(candle.l)
+                            ),
+                            cap = StrokeCap.Butt,
                             strokeWidth = candleWidth * 0.15f
                         )
                         drawLine(
                             if (candle.c > candle.o) greenBrush else redBrush,
-                            start = Offset(x = (index + 0.5f) * candleWidth, y = candleY(candle.o)),
-                            end = Offset(x = (index + 0.5f) * candleWidth, y = candleY(candle.c)),
-                            cap = StrokeCap.Round,
+                            start = Offset(
+                                x = ((index + 0.5f) * candleWidth) - (offset.value * scale),
+                                y = candleY(candle.o)
+                            ),
+                            end = Offset(
+                                x = ((index + 0.5f) * candleWidth) - (offset.value * scale),
+                                y = candleY(candle.c)
+                            ),
+                            cap = StrokeCap.Butt,
                             strokeWidth = candleWidth * 0.8f
                         )
                     }
