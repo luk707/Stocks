493b6691620c9ab9f371ad42289f0f710afc7dff
Luke Harris <6074660+luk707@users.noreply.github.com>
2022-12-09 15:19:47 +0000

    Add price percentage change indicator on stocks screen

--- app/src/main/java/dev/lukeharris/stocks/StocksScreen.kt
+++ app/src/main/java/dev/lukeharris/stocks/StocksScreen.kt
@@ -16,60 +16,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.lukeharris.stocks.ui.theme.Cousine
 import java.text.NumberFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.abs
 
 data class Ticker (
     val ticker: String,
     val name: String,
     val price: Double,
+    val change: Float,
 )
 
 val STOCKS_SCREEN_TEST_DATA = listOf(
     Ticker(
         ticker = "AAPL",
         name = "Apple Inc.",
-        price = 151.15
+        price = 151.15,
+        change = -0.7187f
     ),
     Ticker(
         ticker = "META",
         name = "Meta Platforms, Inc. Class A Common Stock",
-        price = 112.34
+        price = 112.34,
+        change = +0.162f
     ),
     Ticker(
         ticker = "TSLA",
         name = "Tesla, Inc. Common Stock",
-        price = 184.8
+        price = 184.8,
+        change = +0.2049f
     ),
     Ticker(
         ticker = "ASML",
         name = "ASML Holding NV",
-        price = 604.12
+        price = 604.12,
+        change = +0.2271f
     ),
     Ticker(
         ticker = "GME",
         name = "GameStop Corp. Class A",
-        price = 26.75
+        price = 26.75,
+        change = +0.6822f
     ),
     Ticker(
         ticker = "MSFT",
         name = "Microsoft Corp",
-        price = 26.75
+        price = 26.75,
+        change = +0.1586f
     ),
     Ticker(
         ticker = "AMZN",
         name = "Amazon.Com Inc",
-        price = 26.75
+        price = 26.75,
+        change = +0.216f
     ),
     Ticker(
         ticker = "SONY",
         name = "Sony Group Corporation American Depositary Shares (Each Representing One Share of Dollar Validated Common Stock)",
-        price = 26.75
+        price = 26.75,
+        change = -0.7858f
     )
 )
 
@@ -78,8 +87,11 @@ val STOCKS_SCREEN_TEST_DATA = listOf(
 fun StocksScreen(
     navController: NavHostController
 ) {
-    val numberFormat = NumberFormat.getCurrencyInstance(Locale.US)
-    numberFormat.maximumFractionDigits = 2
+    val currencyFormat = NumberFormat.getCurrencyInstance(Locale.US)
+    currencyFormat.maximumFractionDigits = 2
+
+    val percentFormat = NumberFormat.getPercentInstance()
+    percentFormat.maximumFractionDigits = 2
 
     val list: MutableList<Ticker> = ArrayList()
     list.addAll(STOCKS_SCREEN_TEST_DATA)
@@ -116,17 +128,38 @@ fun StocksScreen(
             }
         }
         items(list) { ticker ->
+            val color = if (ticker.change < 0)
+                MaterialTheme.colorScheme.error else
+                MaterialTheme.colorScheme.tertiary
+
+            val textColor = if (ticker.change < 0)
+                MaterialTheme.colorScheme.onError else
+                MaterialTheme.colorScheme.onTertiary
+
             ListItem(
                 modifier = Modifier.clickable { navController.navigate("stocks/${ticker.ticker}") },
                 overlineText = { Text(ticker.name, maxLines = 1, overflow = TextOverflow.Ellipsis) },
                 headlineText = { Text(ticker.ticker, fontFamily = Cousine, fontWeight = FontWeight.Bold) },
-                supportingText = { Text(numberFormat.format(ticker.price), fontFamily = Cousine) },
+                supportingText = {
+                    Row {
+                        Text(currencyFormat.format(ticker.price), fontFamily = Cousine)
+                        Box (
+                            modifier = Modifier
+                                .padding(start = 4.dp)
+                                .clip(RoundedCornerShape(percent = 100))
+                                .background(color)
+                                .padding(horizontal = 4.dp)
+                        ) {
+                            Text(
+                                "${if (ticker.change < 0) "-" else "+"}${percentFormat.format(abs(ticker.change))}",
+                                fontFamily = Cousine,
+                                color = textColor
+                            )
+                        }
+                    }
+                },
                 trailingContent = {
-                    Sparkline(
-                        color = if (ticker.ticker == "META")
-                            MaterialTheme.colorScheme.error else
-                                MaterialTheme.colorScheme.tertiary
-                    )
+                    Sparkline(color = color)
                 }
             )
         }
