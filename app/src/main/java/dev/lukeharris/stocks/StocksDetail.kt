a296786e00a9a6d0f209bfcf6607eeced6bada6b
Luke Harris <6074660+luk707@users.noreply.github.com>
2022-12-09 14:20:37 +0000

    Add range buttons

--- app/src/main/java/dev/lukeharris/stocks/StocksDetail.kt
+++ app/src/main/java/dev/lukeharris/stocks/StocksDetail.kt
@@ -1,34 +1,73 @@
 package dev.lukeharris.stocks
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import dev.lukeharris.stocks.ui.theme.Cousine
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun StocksDetail(ticker: String) {
-    Box(
-        modifier = Modifier
-            .padding(18.dp)
-            .clip(RoundedCornerShape(size = 12.dp))
-            .background(MaterialTheme.colorScheme.surfaceColorAtElevation(
-                NavigationBarDefaults.Elevation))
+    var selectedRange by remember { mutableStateOf("1D") }
 
-    ) {
-        CandlestickChart(
-            modifier = Modifier.padding(10.dp).fillMaxWidth().height(250.dp),
-            bodyColorPriceIncrease = MaterialTheme.colorScheme.tertiary,
-            bodyColorPriceDecrease = MaterialTheme.colorScheme.error,
-            wickColor = MaterialTheme.colorScheme.onSurface
-        )
+    Column {
+        Row(
+            modifier = Modifier
+                .padding(18.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            listOf("1D", "1W", "1M", "3M", "6M", "1Y").forEach { range ->
+                AnimatedContent(targetState =
+                    if (range == selectedRange)
+                        MaterialTheme.colorScheme.surfaceVariant
+                    else
+                        MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0f)
+                ) { bg ->
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(percent = 100))
+                            .background(bg)
+                            .clickable { selectedRange = range }
+                            .padding(PaddingValues(horizontal = 12.dp, vertical = 6.dp))
+                    ) {
+                        Text(range, fontFamily = Cousine)
+                    }
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 18.dp)
+                .clip(RoundedCornerShape(size = 12.dp))
+                .background(
+                    MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        NavigationBarDefaults.Elevation
+                    )
+                )
+
+        ) {
+            CandlestickChart(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(250.dp),
+                bodyColorPriceIncrease = MaterialTheme.colorScheme.tertiary,
+                bodyColorPriceDecrease = MaterialTheme.colorScheme.error,
+                wickColor = MaterialTheme.colorScheme.onSurface
+            )
+        }
     }
 }
